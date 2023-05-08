1ed931bad3e285b4cb92f05d16ce005945cfd1c2
wimtheijken <info@theijken.nl>
2023-05-08 15:37:44 +0200

    uitwerking

--- src/App.js
+++ src/App.js
@@ -1,12 +1,55 @@
 import React from 'react';
+import { ReactComponent as ShoppingCart } from './assets/winkelmandje.svg';
+import citroenen from './assets/citroenen.jpeg';
+import limoenen from './assets/limoenen.png';
+import ijsblokjes from './assets/ijsblokjes.jpg';
+import Product from "./components/Product";
 import './App.css';
 
 function App() {
-  return (
-    <main>
-      Begin hier met de tutorial!
-    </main>
-  );
+    function logClick() {
+        console.log('You clicked!');
+    }
+
+    return (
+        <>
+            <nav>
+                <ul>
+                    <li><a href="/">Shop</a></li>
+                    <li><a href="/">Ons verhaal</a></li>
+                    <li><a href="/">Blog</a></li>
+                </ul>
+                <ShoppingCart className="shopping-cart-icon"/>
+            </nav>
+            <header>
+                <h1>Fruit perfection</h1>
+                <button type="button" onClick={logClick}>
+                    Shop nu
+                </button>
+            </header>
+            <main>
+                <Product
+                    image={citroenen}
+                    title="Citroen"
+                    description="Een citroen is voor de meeste mensen te zuur om zo uit de hand te eten. Van citroen kun je het vruchtvlees, het sap en de schil gebruiken. Het sappige, lichtgele zure vruchtvlees versterkt de smaak van ander voedsel."
+                />
+                <Product
+                    image={limoenen}
+                    title="Limoen"
+                    description="ELimoen is familie van de citroen en de sinaasappel en behoort tot
+                        de citrusvruchten (Wijnruitfamilie). Limoenen zijn rond en kleiner dan citroenen. De schil is
+                        dun, vrij glad en groen"
+                />
+                <Product
+                    image={ijsblokjes}
+                    title="IJsblokjes"
+                    description="Een ijsblokje of ijsklontje is bevroren water in de vorm van een
+                        klein blokje. Het wordt gemaakt in een diepvriezer door water in een plastic vorm te laten
+                        bevriezen."
+                />
+            </main>
+        </>
+    );
 }
 
 export default App;
